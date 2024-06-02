b317b97d4cc9815f465fc43a97c9d3e539f9756d
Sylvain Blanc <sylvain.blanc@dawex.com>
2024-06-02 20:23:03 +0200

    first commit

---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,20 @@
+<script setup>
+</script>
+
+<template>
+  <div class="container">
+    <h1>🎉 30 ans de Sylvain 🎉</h1>
+    <p>Préparez-vous pour la fête la plus épique de l'année !</p>
+    <p>Jean devient un an plus sage et c'est l'occasion de célébrer en grand ! 🎂</p>
+    <p><strong>📅</strong> 22 au 24 Novembre 2024</p>
+    <p><strong>📍</strong> Le Pontet 🏡</p>
+    <p>Plus d'infos à venir</p>
+    <a href="https://www.google.com/calendar/render?action=TEMPLATE&text=Anniversaire+de+Jean&dates=20240620T000000Z/20240620T235900Z&details=Venez+fêter+l'anniversaire+de+Jean!&location=Chez+Jean&sf=true&output=xml" class="link">Ajouter à Google Calendar</a>
+    <div class="sponsors">
+      <h2>Sponsorisé par</h2>
+      <img src="https://via.placeholder.com/150" alt="Sponsor 1">
+      <img src="https://via.placeholder.com/150" alt="Sponsor 2">
+      <img src="https://via.placeholder.com/150" alt="Sponsor 3">
+    </div>
+  </div>
+</template>
