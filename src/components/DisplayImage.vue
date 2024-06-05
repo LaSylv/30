12ca74aa756d75c58deb9625ba7ee3bd73b6d91f
Sylvain Blanc <sylvain.blanc@dawex.com>
2024-06-05 08:18:58 +0200

    add menus

---
+++ src/components/DisplayImage.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <a href="#" @click.prevent="showOverlay = true" class="link">
+      <slot></slot>
+    </a>
+    <div v-if="showOverlay" class="overlay" @click="closeOverlay">
+      <img :src="imageSrc" class="overlay-image" alt="Overlay Image"/>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const props = defineProps({
+  imageSrc: {
+    type: String,
+    required: true
+  }
+});
+
+const showOverlay = ref(false);
+
+const closeOverlay = () => {
+  showOverlay.value = false;
+};
+</script>
+
+<style scoped>
+.link {
+  color: blue;
+  text-decoration: underline;
+  cursor: pointer;
+}
+
+.overlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.8);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 1000;
+}
+
+.overlay-image {
+  max-width: 90%;
+  max-height: 90%;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+  border: 5px solid white;
+}
+</style>
--- src/views/FoodView.vue
+++ src/views/FoodView.vue
@@ -1,10 +1,25 @@
 <script setup>
+import DisplayImage from "@/components/DisplayImage.vue";
+import {ref} from "vue";
+import pizza from "@/assets/food/pizza.png";
+import raclette from "@/assets/food/raclette.png";
+
+const urls = ref({
+  pizza,
+  raclette
+})
 </script>
 
 <template>
   <div>
-    <h2>Nourriture</h2>
-    <h3>En cours de developement, revenez plus tard</h3>
+    <h2>Menus</h2>
+    <DisplayImage :image-src="urls.pizza">
+      <h3>Vendredi Soir</h3>
+    </DisplayImage>
+    <DisplayImage :image-src="urls.raclette">
+      <h3>Samedi Soir</h3>
+    </DisplayImage>
+    <h3>Reste des repas en cours d'élaboration par nos chefs, revenez plus tard</h3>
     <img
         style="width: 100%"
         src="@/assets/wip-2.jpg"
