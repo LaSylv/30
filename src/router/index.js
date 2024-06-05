dc65e7c04ff621f178652cfed31d42d443232825
Sylvain Blanc <sylvain.blanc@dawex.com>
2024-06-05 08:26:18 +0200

    add hasbang for redirct

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,11 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import {createRouter, createWebHashHistory, createWebHistory} from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import ActivitiesView from '../views/ActivitiesView.vue'
 import FoodView from "../views/FoodView.vue";
 import MoneyView from "@/views/MoneyView.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
